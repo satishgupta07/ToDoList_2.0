a14cc7f42f50c7f6b0cc2587e99e251507b259e3
satishgupta07 <satishgupta.cse01@gmail.com>
2020-06-17 01:59:50 +0530

    MongoDB Atlas
    Signed-off-by: satishgupta07 <satishgupta.cse01@gmail.com>

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", { useNewUrlParser: true })
+mongoose.connect("mongodb+srv://admin-satish:Satish123@cluster0-ijgbs.mongodb.net/todolistDB", { useNewUrlParser: true })
 
 const itemsSchema = {
   name: String
